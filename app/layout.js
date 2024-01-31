9a3f947f7ccf98bf721d46b467015cff6817478c
Creigh Battle <creighbattle@Creighs-MacBook-Pro.local>
2024-01-31 10:36:43 -0500

    adding vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Layout from "./components/layout/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "HappyParent",
@@ -9,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Layout>{children}</Layout>
+      <Layout>
+        {children}
+        <Analytics />
+      </Layout>
     </html>
   );
 }
