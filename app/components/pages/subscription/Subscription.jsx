2aa31652c94332ac25d7bcaad533fdcba8b54c32
Creigh Battle <creighbattle@Creighs-MacBook-Pro.local>
2024-02-03 19:52:55 -0500

    updating pricing

--- app/components/pages/subscription/Subscription.jsx
+++ app/components/pages/subscription/Subscription.jsx
@@ -1,6 +1,11 @@
 import useMainStore from "@/app/stores/mainStore";
+import { auth } from "@/firebase";
 import { CheckIcon } from "@heroicons/react/20/solid";
+import { loadStripe } from "@stripe/stripe-js";
+import { getFunctions, httpsCallable } from "firebase/functions";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { ClipLoader } from "react-spinners";
 
 const features = [
   "24/7 AI Assistance",
@@ -20,6 +25,7 @@ export default function Subscription() {
     setShowLearnMore,
   } = useMainStore();
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const handlePartnerClick = () => {
     if (user?.stripeAccountId) {
@@ -40,6 +46,40 @@ export default function Subscription() {
     }, 1200);
   };
 
+  const continueToPayment = async () => {
+    setLoading(true);
+    const functions = getFunctions();
+    const continueToPaymentFn = httpsCallable(functions, "continueToPayment");
+    const stripePromise = loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+    );
+
+    const stripeCustomerId = user?.stripeCustomerId
+      ? user.stripeCustomerId
+      : null;
+
+    try {
+      const response = await continueToPaymentFn({
+        uid: auth.currentUser.uid,
+        stripeCustomerId,
+      });
+      const sessionId = response.data.sessionId;
+      const stripe = await stripePromise;
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      console.log(error);
+      if (error.message == "Referral code does not exist.") {
+        setInvalidCode(true);
+      } else {
+        alert(
+          "Oops, we encountered an error while trying to create your checkout session. Please try again."
+        );
+      }
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className="mx-auto items-center">
       <div className="bg-white sm:mx-8 lg:mx-0 rounded-t-3xl  rounded-3xl  px-2 py-4 ">
@@ -48,7 +88,7 @@ export default function Subscription() {
           <p className="mt-4 flex items-baseline gap-x-1">
             <span className="text-gray-900 text-base font-regular tracking-tight">
               {user?.affiliateCode && "$10"}
-              {!user?.affiliateCode && "$25"}
+              {!user?.affiliateCode && "$7.99"}
             </span>
             <span className="text-gray-500 text-base">/</span>
             <span className="text-gray-500 text-base">month</span>
@@ -73,10 +113,11 @@ export default function Subscription() {
           ))}
         </ul>
         <div
-          onClick={() => setSubscriptionScreen(2)}
+          onClick={continueToPayment}
           className="text-gray-600 border border-[#88D8DF] mt-8 block rounded-md py-2.5 px-3.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 cursor-pointer"
         >
-          Get Started Today
+          {!loading && "Get Started Today"}
+          {loading && <ClipLoader size={20} color="#F2D1DC" />}
         </div>
 
         <div
@@ -86,13 +127,13 @@ export default function Subscription() {
           Learn More
         </div>
 
-        <div
+        {/* <div
           onClick={handlePartnerClick}
           className="text-gray-600 border border-[#F2D1DC] mt-2 block rounded-md py-2.5 px-3.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 cursor-pointer"
         >
           {user?.stripeAccountId && "Partner Dashboard"}
           {!user?.stripeAccountId && "Become a Partner"}
-        </div>
+        </div> */}
       </div>
     </div>
   );
